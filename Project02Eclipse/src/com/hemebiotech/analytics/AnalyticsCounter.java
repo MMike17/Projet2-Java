f9e7a4d51a5a20fce998e6e2ee70e24b764afe17
Mike <mmatthews@manzalab.com>
2020-12-23 13:55:12 +0100

    Added .gitignore file and formatted code

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,33 +5,31 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
+	private static int headacheCount = 0; // initialize to 0
+	private static int rashCount = 0; // initialize to 0
+	private static int pupilCount = 0; // initialize to 0
+
 	public static void main(String args[]) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
 		while (line != null) {
 			if (line.equals("headache")) {
 				headacheCount++;
-			}
-			else if (line.equals("rash")) {
+			} else if (line.equals("rash")) {
 				rashCount++;
-			}
-			else if (line.contains("pupils")) {
+			} else if (line.contains("pupils")) {
 				pupilCount++;
 			}
 
-			line = reader.readLine();	// get another symptom
+			line = reader.readLine(); // get another symptom
 		}
 
 		reader.close();
-		
+
 		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
+		FileWriter writer = new FileWriter("result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
